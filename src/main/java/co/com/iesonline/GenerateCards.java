121d54fef694bde75603791f39f3e01d57af4f78
b3tancur343 <betancur343.ies@gmail.com>
2018-11-19 11:09:59 -0500

    style: refactoring and adding documentation for GenerateCards class

--- src/main/java/co/com/iesonline/GenerateCards.java
+++ src/main/java/co/com/iesonline/GenerateCards.java
@@ -20,19 +20,19 @@ import com.itextpdf.layout.property.TextAlignment;
 
 /**
  * 
- * @author Betancur
+ * @author B3tancur343
  *
  */
 public class GenerateCards {
 	
-	String FONT = "./src/main/resources/fonts/a_AlternaOtl.ttf";
+	public static final String FONT = "./src/main/resources/fonts/a_AlternaOtl.ttf";
 	
-	public static final String DEST = "results/bingo_cards.pdf";
-	public static final String DEST1 = "results/bingo_conditions.pdf";
+	public static final String CARDS_PDF_LOCATION = "results/bingo_cards.pdf";
+	public static final String CONDITIONS_PDF_LOCATION = "results/bingo_conditions.pdf";
 
-	public static final String CARDIMAGE = "./src/main/resources/img/Bingo Poderoso (Tabla).png";
-	public static final String WPLAYIMAGE = "./src/main/resources/img/wplay_logo.png";
-	public static final String CONDITIONIMAGE = "./src/main/resources/img/Bingo Condiciones.png";
+	public static final String CARD_IMAGE_LOCATION = "./src/main/resources/img/Bingo Poderoso (Tabla).png";
+	public static final String ICON_LOCATION = "./src/main/resources/img/wplay_logo.png";
+	public static final String CONDITIONS_IMAGE_LOCATION = "./src/main/resources/img/Bingo Condiciones.png";
 
 	/**
 	 * 
@@ -40,13 +40,14 @@ public class GenerateCards {
 	 * @throws Exception
 	 */
 	public static void main(String[] args) throws Exception {
-		File file = new File(DEST);
-		file.getParentFile().mkdirs();
-		new GenerateCards().manipulateCardsPdf(DEST);
 		
-//		File file1 = new File(DEST1);
-//		file1.getParentFile().mkdirs();
-//		new GenerateCards().manipulateConditionsPdf(DEST1);
+		File file = new File(CARDS_PDF_LOCATION);
+		file.getParentFile().mkdirs();
+		new GenerateCards().manipulateCardsPdf(CARDS_PDF_LOCATION);
+		
+		File file1 = new File(CONDITIONS_PDF_LOCATION);
+		file1.getParentFile().mkdirs();
+		new GenerateCards().manipulateConditionsPdf(CONDITIONS_PDF_LOCATION);
 	}
 
 	/**
@@ -55,49 +56,56 @@ public class GenerateCards {
 	 * @throws Exception
 	 */
 	protected void manipulateCardsPdf(String dest) throws Exception {
+		
 		PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
 		PageSize pageSize = new PageSize(PageSize.A4);
 
 		Document doc = new Document(pdfDoc, pageSize);
 		doc.setMargins(0, 0, 0, 0);
 
-		Image cardImage = new Image(ImageDataFactory.create(CARDIMAGE));      
+		Image cardImage = new Image(ImageDataFactory.create(CARD_IMAGE_LOCATION));      
         cardImage.setAutoScale(true);
-		Image wplayImage = new Image(ImageDataFactory.create(WPLAYIMAGE));	    
+		Image wplayImage = new Image(ImageDataFactory.create(ICON_LOCATION));	    
 		wplayImage.setAutoScale(true);
-//		Image conditionsImage = new Image(ImageDataFactory.create(CONDITIONIMAGE));    
-//		conditionsImage.setAutoScale(true);
 		
 		Table ballsTable = getBallsTable(wplayImage);
 		Paragraph tableNum = new Paragraph("12521");
 		tableNum.setFontSize(20);
 		
-		int cardsTotal = 10;
-		for (int p = 0; p < cardsTotal/2; p++) {
-	        pdfDoc.addNewPage(new PageSize(pageSize));	        
+		//TODO Set total cards.
+		int totalCards = 10;
+		
+		//TODO Set width and coordinates of balls table before generate pdf's.
+		int tableWidth = 210;
+		int xCard1 = 185;
+		int yCard1 = 548;
+		int xCard2 = xCard1;
+		int yCard2 = 148;
+		
+		for (int p = 0; p < totalCards/2; p++) {
+	        
+			pdfDoc.addNewPage(new PageSize(pageSize));	        
 	    
-	        for (int i = 0; i < 2; i++) {
-	    		
-	    		int tableWidth = 210;
-	    		int x = 185;
-	    		int y1 = 548;
-	    		int y2 = 148;
-	    		
+	        for (int i = 0; i < 2; i++) {    		
+
+	    	    //card1	    	    
     			cardImage.setFixedPosition(p + 1, 0, 450);				
-				ballsTable.setFixedPosition(p + 1, x, y1, tableWidth);
+				ballsTable.setFixedPosition(p + 1, xCard1, yCard1, tableWidth);
 				ballsTable.setHeight(100);
 	    	    doc.add(cardImage);
 	    	    doc.add(ballsTable);
-	    	    
-	    	    cardImage.setFixedPosition(p + 1, 0, 50);    		
-	    		ballsTable.setFixedPosition(p + 1, x, y2, tableWidth);
-	    	    doc.add(cardImage);
-	    	    doc.add(ballsTable);
-	    	    
+
 	    	    tableNum.setRotationAngle(1.57);
 	    	    tableNum.setFixedPosition(p + 1, 35, 536, 200);
 	    	    doc.add(tableNum);
 	    	    
+	    	    //card2	    	    
+	    	    cardImage.setFixedPosition(p + 1, 0, 50);    		
+	    		ballsTable.setFixedPosition(p + 1, xCard2, yCard2, tableWidth);
+	    	    doc.add(cardImage);
+	    	    doc.add(ballsTable);
+	    	    
+//	    	    tableNum.setRotationAngle(1.57);
 	    	    tableNum.setFixedPosition(p + 1, 35, 136, 200);
 	    	    doc.add(tableNum);
 		    }
@@ -112,13 +120,14 @@ public class GenerateCards {
 	 * @throws Exception
 	 */
 	protected void manipulateConditionsPdf(String dest) throws Exception {
+		
 		PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
 		PageSize pageSize = new PageSize(PageSize.A4);
 
 		Document doc = new Document(pdfDoc, pageSize);
 		doc.setMargins(0, 0, 0, 0);
 
-		Image conditionsImage = new Image(ImageDataFactory.create(CONDITIONIMAGE));    
+		Image conditionsImage = new Image(ImageDataFactory.create(CONDITIONS_IMAGE_LOCATION));    
 		conditionsImage.setAutoScale(true);
 		
 		int cardsTotal = 10;
@@ -134,16 +143,24 @@ public class GenerateCards {
 		
 		doc.close();		
 	}
-	
-	static int[] joinArray(int[]... arrays) {
-        int length = 0;
+
+	/**
+	 * 
+	 * @param arrays
+	 * @return
+	 */
+	public static int[] joinArray(int[]... arrays) {
+        
+		int length = 0;
+        int offset = 0;
+        
+
         for (int[] array : arrays) {
             length += array.length;
         }
 
         final int[] result = new int[length];
-
-        int offset = 0;
+        
         for (int[] array : arrays) {
             System.arraycopy(array, 0, result, offset, array.length);
             offset += array.length;
@@ -152,10 +169,13 @@ public class GenerateCards {
         return result;
     }
 	
-	public String[] organizar(int[] lista) {
-		
-//		System.out.println(Arrays.toString(lista));
-		
+	/**
+	 * 
+	 * @param lista
+	 * @return
+	 */
+	public String[] realignTableIds(int[] lista) {
+				
 		int[] columna1 = Arrays.copyOfRange(lista, 0, 5);
 		int[] columna2 = Arrays.copyOfRange(lista, 5, 10);
 		int[] columna3 = Arrays.copyOfRange(lista, 10, 14);
@@ -170,12 +190,14 @@ public class GenerateCards {
 		
 		int[] concatAll = joinArray(columna5,columna4,columna3, columna2, columna1);
 //		System.out.println(Arrays.toString(concatAll));
+		
 		String[] result = new String[24];
 		
 		for (int i = 0; i < concatAll.length; i++) {
 			String value = String.valueOf(concatAll[i]);
 			result[i] = value;
 		}
+		
 		return result;
 	}
 	
@@ -187,13 +209,11 @@ public class GenerateCards {
 	 */
 	private Table getBallsTable(Image centerImg) throws Exception {
 		
-//		PdfFont font = PdfFontFactory.createFont(FontConstants.HELVETICA_BOLD);
-//		PdfFont font = PdfFontFactory.createFont(FONT);
 		PdfFont font = PdfFontFactory.createFont(FONT, PdfEncodings.WINANSI, true);		
         
         int[] balls = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23}; 
         
-        String[] organizar = organizar(balls);
+        String[] organizar = realignTableIds(balls);
         System.out.println("organizar:" + Arrays.toString(organizar));
         
 		Table ballsTable = new Table(5);
@@ -241,5 +261,5 @@ public class GenerateCards {
 		
 		return ballsTable;
 	}
-
+	
 }
